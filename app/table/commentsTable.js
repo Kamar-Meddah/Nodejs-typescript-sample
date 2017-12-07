9b4329354cd46896470872ccd9d32e9021ca2970
kamar Meddah <kamar.meddah@gmail.com>
2017-12-07 18:18:00 +0100

    create commentsTable Class

---
+++ app/table/commentsTable.js
@@ -0,0 +1,24 @@
+const table = require ('../../core/table/table');
+
+module.exports = class commentsTable extends table{
+
+    constructor(){
+        super();
+        this.tab='comments';
+    }
+
+    find(id,cb){
+        this[this.tab].findAll({where:{"articleId":id},order: [['date', 'DESC']]}).then(res=>{
+            cb(res)
+        })
+    }
+
+    deleteCom(id,cb=null){
+        this[this.tab].destroy({where:{"articleId":id}}).then(res=>{
+            if(cb !== null){
+                cb();
+            }
+        })
+    }
+
+}
