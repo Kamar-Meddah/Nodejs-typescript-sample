3c18d0bd4ecafde4bfb40dff8319aa42ac58a795
kamar Meddah <kamar.meddah@gmail.com>
2017-12-07 18:18:24 +0100

    create imagesTable Class

---
+++ app/table/imagesTable.js
@@ -0,0 +1,25 @@
+const table = require ('../../core/table/table');
+
+module.exports = class imagesTable extends table{
+
+    constructor(){
+        super();
+        this.tab='images';
+    }
+
+   
+   findImg(id,cb){
+       this[this.tab].findAll({where:{"articleId":id}}).then((res)=>{
+           cb(res)
+       })
+    }
+
+    deleteWithArticle(id,cb=null){
+        this[this.tab].destroy({where:{"articleId":id}}).then(res=>{
+            if(cb !== null){
+                cb();
+            }
+        })
+
+    }
+}
