2adc1d40731b153fe984215d486a1e1c246eacfc
kamar Meddah <kamar.meddah@gmail.com>
2017-12-07 17:54:31 +0100

    create a singleton app class

---
+++ app/app.js
@@ -0,0 +1,35 @@
+class app{//Begin Class
+
+     static getInstance(){
+         if(this.instance === undefined){
+             this.instance = new app();
+         }
+         return this.instance;
+     }
+
+     getDb(){
+         
+        if(this.DB=== undefined){
+            const dbConnect=require('../core/database/SqlDB');
+            this.DB=new dbConnect(require('../config/dbconfig'));
+        }
+        return this.DB.getDbConnect();
+    }
+
+    getTable(name){
+        let n=name+'Table';
+        let ins = require('./table/'+n);
+        return new ins();
+    }
+
+    getDbAuth(){
+        
+       if(this.dbAuth=== undefined){
+           const auth=require('../core/admin/dbAuth');
+           this.dbAuth=new auth();
+       }
+       return this.dbAuth;
+   }
+
+}//END Class
+module.exports=app.getInstance();
