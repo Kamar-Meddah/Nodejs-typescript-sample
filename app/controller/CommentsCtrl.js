22ba42ab651c3f8ad6a4223f490e4c77c7ebe60a
kamar Meddah <kamar.meddah@gmail.com>
2017-12-07 18:19:42 +0100

    create CommentsCtrl Class

---
+++ app/controller/CommentsCtrl.js
@@ -0,0 +1,23 @@
+module.exports = class CommentsCtrl {
+    constructor() {
+        this.comments = require('../app').getTable('comments');
+    }
+
+    find(request, response) {
+        this.comments.find(request.body.id, (row) => {
+            response.json(row)
+        });
+    }
+
+    add(request, response) {
+        this.comments.create({ "name": request.body.name, "content": request.body.comment, "articleId": request.body.postId }, (data) => {
+            response.json({ 'id': data });
+        });
+    }
+
+    delete(request, response) {
+        this.comments.delete(request.body.id);
+        response.json({});
+    }
+
+}
